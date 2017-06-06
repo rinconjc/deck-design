fa5cb3ae638878962a81f1e265763ad63b0cbc6b
Julio Rincon <jcrinconc@gmail.com>
2017-06-07 07:51:07 +1000

    stairs

--- src/cadtoy/core.clj
+++ src/cadtoy/core.clj
@@ -9,19 +9,23 @@
 (def decking-color #(m/color [(/ 140 255) (/ 70 255) (/ 23 255) 1] %))
 (def concrete-color #(m/color [(/ 0xcc 255) (/ 0xcc 255) (/ 0xcc 255) 1] %))
 
+(def left-width 132)
 (def depth 400)
 (def board-size [8.6 1.9])
 (def left-depth (+ depth 110))
 (def right-depth (- depth 90))
 (def width 632)
 (def height (+ 100 9 1.9))
-(def kitchen-lc (- width 132))
+(def kitchen-lc (- width left-width))
 (def kitchen-rc (- kitchen-lc 330))
 
 (defn balcony-floor []
-  (->> (cube (- width kitchen-rc) 160 15 :center false)
-       (m/translate [kitchen-rc (- depth 160) -15])
-       concrete-color))
+  (concrete-color
+   (union
+    (->> (cube (- width kitchen-rc) 160 15 :center false)
+         (m/translate [kitchen-rc (- depth 160) -15]))
+    (->> (cube left-width 110 15 :center false)
+         (m/translate [kitchen-lc depth -15])))))
 
 (defn post [x y z]
   (->> (cube 9 9 (+ height 10 (Math/abs z)) :center false)
@@ -39,7 +43,7 @@
        treated-pine-color))
 
 (def bearer
-  (->> (cube 632 4.5 19 :center false)
+  (->> (cube (+ width 9) 4.5 19 :center false)
        (m/translate [0 0 -10])
        treated-pine-color))
 
@@ -81,41 +85,48 @@
          (decking kitchen-rc y (- width kitchen-rc) [w h])
          (decking 0 y width [w h])))
      (for [y (range left-depth depth (- 0 w gap))]
-       (decking kitchen-lc y 132 [w h])))))
+       (decking kitchen-lc y left-width [w h])))))
 
 (defn joists []
-  (concat
-   (for [x (range 4)] (joist (* 45 x) right-depth))
-   (for [x (range 7)] (joist (+ kitchen-rc (* 45 x)) depth))
-   (for [x (range 3)] (joist (+ kitchen-lc (* 45 x)) left-depth))))
+  (apply union
+         (joist (+ width 4.5) left-depth)
+         (joist (+ width 45) 90)
+         (for [x (range 4)] (joist (* 45 x) right-depth))
+         (for [x (range 7)] (joist (+ kitchen-rc (* 45 x)) depth))
+         (for [x (range 3)] (joist (+ kitchen-lc (* 45 x)) left-depth))))
+
+(defn stairs-south []
+  (post width depth ))
 
 (def deck
-  (apply union
-         (->> (m/polygon [[0 0] [width 0] [width left-depth] [(- width 132) left-depth]
-                          [kitchen-lc depth] [kitchen-rc depth]
-                          [kitchen-rc right-depth] [0 right-depth]])
-              (m/color [0.9 0.9 0.9 0.3]))
-         (balcony-floor)
-         (post 0 0 -110)
-         (post (- width 9) 0 -150)
-         (post 210 0 -120)
-         (post 420 0 -130)
-         (post 0 (- right-depth 9) -120)
-         (post kitchen-rc (- right-depth 9) -120)
-         (post (- width 9) (- left-depth 90 9) -150)
-         bearer
-         (ledger 0 right-depth 170)
-         (ledger kitchen-rc depth 330)
-         (ledger kitchen-lc left-depth 132)
-         (joist (- width 4.5) left-depth)
-         (hand-rail-x 0 0 width)
-         (hand-rail-x 0 (- right-depth 4.5) 170)
-         (hand-rail-y 0 0 right-depth)
-         (hand-rail-y (- width 4.5) 0 (- left-depth 90 9))
-         (stringer-x 0 kitchen-rc right-depth 110 90)
-         (joists)
-         (decking-board)
-         ))
+  (union
+   ;; (->> (m/polygon [[0 0] [width 0] [width left-depth] [(- width left-width) left-depth]
+   ;;                  [kitchen-lc depth] [kitchen-rc depth]
+   ;;                  [kitchen-rc right-depth] [0 right-depth]])
+   ;;      (m/color [0.9 0.9 0.9 0.3]))
+   (balcony-floor)
+   (post 0 0 -110)
+   (post width 0 -150)
+   (post 210 0 -120)
+   (post 420 0 -130)
+   (post 0 (- right-depth 9) -120)
+   (post kitchen-rc (- right-depth 9) -120)
+   (post width (- left-depth 90 9) -150)
+   (post (+ 90 width) (- left-depth 90 9) -150) ;; south-stair-south-post
+   (post (+ 90 (- width 9)) (- left-depth 9) -150)
+   bearer
+   (ledger 0 right-depth 170)
+   (ledger kitchen-rc depth 330)
+   (ledger kitchen-lc left-depth (+ left-width 90))
+   (ledger width (- left-depth 90) 90)
+   (hand-rail-x 0 0 width)
+   (hand-rail-x 0 (- right-depth 4.5) 170)
+   (hand-rail-y 0 0 right-depth)
+   (hand-rail-y (- width 4.5) 0 (- left-depth 90 9))
+   (stringer-x 0 kitchen-rc right-depth 110 90)
+   (joists)
+   ;;(decking-board)
+   ))
 
 (spit "post-demo.scad"
       (write-scad deck))
